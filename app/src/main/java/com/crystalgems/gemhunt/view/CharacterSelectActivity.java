d05f1bdc528b7a38d22e5ecd1e121d43671ae149
Alex Buleon <alex.buleon@gmail.com>
2017-01-02 23:36:13 +0100

    Added a character selection activity (blank for now)
    Added the transparent activity

---
+++ app/src/main/java/com/crystalgems/gemhunt/view/CharacterSelectActivity.java
@@ -0,0 +1,11 @@
+package com.crystalgems.gemhunt.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CharacterSelectActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
