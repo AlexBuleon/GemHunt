51e7e384992366c74492fa0275661fceb0e57074
Antoine Chédin <antoine.chedin@gmail.com>
2017-01-02 19:08:21 +0100

    Minor fix

--- app/src/main/java/com/crystalgems/gemhunt/model/DicePool.java
+++ app/src/main/java/com/crystalgems/gemhunt/model/DicePool.java
@@ -4,18 +4,27 @@ import java.util.ArrayList;
 
 public class DicePool extends ArrayList<Dice> {
 
-	public DicePool(){
-	}
-	
-	public DicePool(Dice[] dices){
-		for(int i = 0; i < dices.length; i++){
-			this.add(dices[i]);
-		}
-	}
-	
-	
+    public DicePool() {
+    }
+
+    public DicePool(Dice[] dices) {
+        for (int i = 0; i < dices.length; i++) {
+            this.add(dices[i]);
+        }
+    }
+
+
     public void moveTo(int index, DicePool dicePool) {
         dicePool.add(this.get(index));
         this.remove(index);
+    }
+
+    public void moveTo(DicePool dicePool) {
+        for (int i = 0; i < this.size(); i++) {
+            dicePool.add(this.get(i));
+            this.remove(i);
+        }
+
+
     }
 }
