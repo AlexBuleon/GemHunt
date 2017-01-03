fcd4e8591ebd3f71fecc7ad1c4c93a1dc26be251
mroklom <valentin.marechal05@gmail.com>
2017-01-03 20:19:15 +0100

    change PlayerSchema class
    add the pictureId to the schema

--- app/src/main/java/com/crystalgems/gemhunt/database/schema/PlayerSchema.java
+++ app/src/main/java/com/crystalgems/gemhunt/database/schema/PlayerSchema.java
@@ -7,6 +7,7 @@ public interface PlayerSchema {
 
     String COLUMN_PLAYER_ID = "id";
     String COLUMN_PLAYER_NAME = "name";
+    String COLUMN_PLAYER_PICTURE_ID = "pictureId";
     String COLUMN_PLAYER_TOTAL_SCORE = "totalScore";
     String COLUMN_PLAYER_PENALTY = "penalty";
 
@@ -15,10 +16,11 @@ public interface PlayerSchema {
             PLAYER_TABLE + "(" +
             COLUMN_PLAYER_ID + " integer primary key autoincrement," +
             COLUMN_PLAYER_NAME + " texte not null," +
+            COLUMN_PLAYER_PICTURE_ID + " integer not null," +
             COLUMN_PLAYER_TOTAL_SCORE + " integer not null," +
             COLUMN_PLAYER_PENALTY + " integer not null" +
             ");";
 
     //List of the column of the table
-    String[] PLAYER_COLUMNS = new String[] { COLUMN_PLAYER_ID, COLUMN_PLAYER_NAME, COLUMN_PLAYER_TOTAL_SCORE, COLUMN_PLAYER_PENALTY};
+    String[] PLAYER_COLUMNS = new String[] { COLUMN_PLAYER_ID, COLUMN_PLAYER_NAME, COLUMN_PLAYER_PICTURE_ID, COLUMN_PLAYER_TOTAL_SCORE, COLUMN_PLAYER_PENALTY};
 }
