81d8880329a3a05254c5de26b3ba56deb92f1bce
mroklom <valentin.marechal05@gmail.com>
2017-01-03 20:38:06 +0100

    change GamesDAO and PlayerDAO
    implements the mandatory methods

--- app/src/main/java/com/crystalgems/gemhunt/dao/GameDAO.java
+++ app/src/main/java/com/crystalgems/gemhunt/dao/GameDAO.java
@@ -1,11 +1,16 @@
 package com.crystalgems.gemhunt.dao;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class GameDAO extends DatabaseContentProvider {
 
-
     public GameDAO(SQLiteDatabase database) {
         super(database);
+    }
+
+    @Override
+    protected <T> T cursorToEntity(Cursor cursor) {
+        return null;
     }
 }
--- app/src/main/java/com/crystalgems/gemhunt/dao/PlayerDAO.java
+++ app/src/main/java/com/crystalgems/gemhunt/dao/PlayerDAO.java
@@ -1,10 +1,16 @@
 package com.crystalgems.gemhunt.dao;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class PlayerDAO extends DatabaseContentProvider {
 
     public PlayerDAO(SQLiteDatabase database) {
         super(database);
+    }
+
+    @Override
+    protected <T> T cursorToEntity(Cursor cursor) {
+        return null;
     }
 }
