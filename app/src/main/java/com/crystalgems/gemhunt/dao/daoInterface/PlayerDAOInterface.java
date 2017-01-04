569ca80934473ea50622ac08379469bbff6e5e26
mroklom <valentin.marechal05@gmail.com>
2017-01-04 19:50:28 +0100

    Change PlayerDAO
    correct typo

--- app/src/main/java/com/crystalgems/gemhunt/dao/daoInterface/PlayerDAOInterface.java
+++ app/src/main/java/com/crystalgems/gemhunt/dao/daoInterface/PlayerDAOInterface.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface PlayerDAOInterface {
     Player findPlayerById(int id);
-    Player findPlaerByName(String name);
+    Player findPlayerByName(String name);
     List<Player> findAllPlayer();
     boolean addPlayer(Player player);
     boolean addPlayers(List<Player> players);
