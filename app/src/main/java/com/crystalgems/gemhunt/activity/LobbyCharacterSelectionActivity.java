3a4255e5f7831e7e9db1e1978f92ddfafc74df94
Alex Buleon <alex.buleon@gmail.com>
2017-01-04 14:52:00 +0100

    Add character selection activity java class
     Add activity
    change layout
    Add intent in the lobby fragment
    delete the old CharacterSelectActivity.java

--- app/src/main/java/com/crystalgems/gemhunt/activity/LobbyCharacterSelectionActivity.java
+++ app/src/main/java/com/crystalgems/gemhunt/activity/LobbyCharacterSelectionActivity.java
@@ -1,4 +1,44 @@
 package com.crystalgems.gemhunt.activity;
 
-public class LobbyCharacterSelectionActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.apps.su.gemhunt.R;
+
+public class LobbyCharacterSelectionActivity extends Activity implements View.OnClickListener {
+
+    private Button steven;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.character_select_activity);
+
+        intent = getIntent();
+
+        steven = (Button) findViewById(R.id.stevenButton);
+
+        steven.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == steven) {
+            intent.putExtra("character", 1);
+        }
+
+        this.finish();
+    }
 }
