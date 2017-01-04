b43a0e9cbd0a7e700fad40bae541f68a01104b68
mroklom <valentin.marechal05@gmail.com>
2017-01-04 19:04:39 +0100

    Change database model

--- app/src/main/java/com/crystalgems/gemhunt/database/schema/PlayerSchema.java
+++ app/src/main/java/com/crystalgems/gemhunt/database/schema/PlayerSchema.java
@@ -8,8 +8,8 @@ public interface PlayerSchema {
     String COLUMN_PLAYER_ID = "id";
     String COLUMN_PLAYER_NAME = "name";
     String COLUMN_PLAYER_PICTURE_ID = "pictureId";
-    String COLUMN_PLAYER_TOTAL_SCORE = "totalScore";
-    String COLUMN_PLAYER_TOTAL_PENALTY = "totalPenalty";
+    String COLUMN_PLAYER_GLOBAL_SCORE = "globalScore";
+    String COLUMN_PLAYER_GLOBAL_PENALTY = "globalPenalty";
 
     //Create table query
     String PLAYER_TABLE_CREATE = "create table if not exists " +
@@ -17,8 +17,8 @@ public interface PlayerSchema {
             COLUMN_PLAYER_ID + " integer primary key autoincrement," +
             COLUMN_PLAYER_NAME + " text not null," +
             COLUMN_PLAYER_PICTURE_ID + " integer not null," +
-            COLUMN_PLAYER_TOTAL_SCORE + " integer not null," +
-            COLUMN_PLAYER_TOTAL_PENALTY + " integer not null" +
+            COLUMN_PLAYER_GLOBAL_SCORE + " integer not null," +
+            COLUMN_PLAYER_GLOBAL_PENALTY + " integer not null" +
             ");";
 
     //List of the column of the table
@@ -26,7 +26,7 @@ public interface PlayerSchema {
             COLUMN_PLAYER_ID,
             COLUMN_PLAYER_NAME,
             COLUMN_PLAYER_PICTURE_ID,
-            COLUMN_PLAYER_TOTAL_SCORE,
-            COLUMN_PLAYER_TOTAL_PENALTY
+            COLUMN_PLAYER_GLOBAL_SCORE,
+            COLUMN_PLAYER_GLOBAL_PENALTY
     };
 }
