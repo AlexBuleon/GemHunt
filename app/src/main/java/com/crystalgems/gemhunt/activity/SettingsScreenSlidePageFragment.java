e31c22e0a3d722935394855c94a9a06c1889508a
Antoine Chédin <antoine.chedin@gmail.com>
2017-01-04 13:48:03 +0100

    Add click listener on credit button

--- app/src/main/java/com/crystalgems/gemhunt/activity/SettingsScreenSlidePageFragment.java
+++ app/src/main/java/com/crystalgems/gemhunt/activity/SettingsScreenSlidePageFragment.java
@@ -1,14 +1,18 @@
 package com.crystalgems.gemhunt.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.apps.su.gemhunt.R;
 
-public class SettingsScreenSlidePageFragment extends Fragment {
+public class SettingsScreenSlidePageFragment extends Fragment implements View.OnClickListener {
+
+    private Button creditButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,7 +20,16 @@ public class SettingsScreenSlidePageFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_settings_screen_slide_page, container, false);
 
+        creditButton = (Button) rootView.findViewById(R.id.credits_button);
+        creditButton.setOnClickListener(this);
+
         return rootView;
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == creditButton) {
+            startActivity(new Intent(this.getActivity(), CreditsActivity.class));
+        }
+    }
 }
