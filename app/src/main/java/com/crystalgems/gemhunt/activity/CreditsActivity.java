db686c73fed2c3ef05227eb2a012637e22f7b68f
mroklom <valentin.marechal05@gmail.com>
2017-01-04 13:39:34 +0100

    add CreditsActivity

---
+++ app/src/main/java/com/crystalgems/gemhunt/activity/CreditsActivity.java
@@ -0,0 +1,15 @@
+package com.crystalgems.gemhunt.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.su.gemhunt.R;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
