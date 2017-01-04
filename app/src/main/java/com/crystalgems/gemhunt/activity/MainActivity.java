ac01f39b8c3cbf628b872ade8e9bfa133b9f4790
Antoine Chédin <antoine.chedin@gmail.com>
2017-01-04 11:14:52 +0100

    Delete 2 blank row

--- app/src/main/java/com/crystalgems/gemhunt/activity/MainActivity.java
+++ app/src/main/java/com/crystalgems/gemhunt/activity/MainActivity.java
@@ -21,10 +21,7 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-
         setContentView(R.layout.activity_screen_slide);
 
         //ViewPager and PageAdapter
